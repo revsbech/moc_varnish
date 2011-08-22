df557136a179d7e7a9b08689a5b04f89a34a8c17
www-data <www-data@mocsystems.com>
2011-08-22 22:09:52 +0200

    [+BUGFIX] Session is not written when storeSessionData is called
    Although we have a special check for storeSessionData being called
    when we determine if the cookie should be set or not, we are doing the
    check (way) to late in the bootstrapping. So now we manually call
    setSessinCookie (but only once) whenever the storeSessionData is
    called and a change to sesData is detected.
    
    Change-Id: I5bc8a3a17559103c289b8c7979ec7424236e2a15

--- xclass/class.ux_tslib_feuserauth.php
+++ xclass/class.ux_tslib_feuserauth.php
@@ -1,24 +1,35 @@
 <?php
 /**
  *
- * The class ocerrides the default dontSetCookie value of tslib_feuserAuth. This way a new cookie is never
- * set unless explicityly asked for (by setting dontSetCookie to FALSE.
- * This is done when creating new FE user sessions, and when writing stuff in the sessionData for
- * GLOBALS['TSFE']->fe_user.
+ * The class overrides the default dontSetCookie value of tslib_feUserAuth. This way a new cookie is never
+ * set unless explicitly asked for.
+ * It changes the property dontSetCookie to be TRUE by default. And manually call setSessionCookie
+ * after calling storeSessionData (if there was a change to session data). The same goes for
+ * createUserSession.
  *
  */
 class ux_tslib_feuserAuth extends tslib_feUserAuth {
 	public $dontSetCookie = TRUE;
 
+	public $cookieIsSet = FALSE;
+
 	function storeSessionData() {
 		if ($this->sesData_change) {
-			$this->dontSetCookie = FALSE;
+			$this->setSessionCookie();
 		}
 		parent::storeSessionData();
 	}
 
 	function createUserSession($tempuser) {
-		$this->dontSetCookie = FALSE;
+		$this->setSessionCookie();
 		parent::createUserSession($tempuser);
 	}
+
+	protected function setSessionCookie() {
+		if ($this->cookieIsSet === FALSE) {
+			parent::setSessionCookie();
+			$this->cookieIsSet = TRUE;
+		}
+	}
+
 }
