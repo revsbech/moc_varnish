16e8701f4605ba90a78108fd9700589abedef6e7
Michael Stucki <michael.stucki@typo3.org>
2011-10-07 19:02:22 +0200

    Add support for caching framework
    Change-Id: I9e4ab2fd57e58352764fd2a3ca3a85bf9c3bd6dc

--- user_renderInt.php
+++ user_renderInt.php
@@ -23,6 +23,50 @@ function user_renderint($content,$conf) {
 		}
 	}
 
+	// TODO: fix alignment (next change)
+	if (TRUE) {
+
+			//Copied from tslib_fe
+		/**
+		 * @todo bring to sync with 4.5 code
+		 */
+		if($data['INTincScript'][$key]) {
+			$INTiS_cObj = unserialize($data['INTincScript'][$key]['cObj']);
+			/* @var $INTiS_cObj tslib_cObj */
+			$INTiS_cObj->INT_include=1;
+			switch($data['INTincScript'][$key]['type'])	{
+				case 'SCRIPT':
+					$incContent = $INTiS_cObj->PHP_SCRIPT($data['INTincScript'][$key]['conf']);
+				break;
+				case 'COA':
+					$incContent = $INTiS_cObj->COBJ_ARRAY($data['INTincScript'][$key]['conf']);
+				break;
+				case 'FUNC':
+					$incContent = $INTiS_cObj->USER($data['INTincScript'][$key]['conf']);
+				break;
+				case 'POSTUSERFUNC':
+					$incContent = $INTiS_cObj->callUserFunction($data['INTincScript'][$key]['postUserFunc'], $data['INTincScript'][$key]['conf'], $data['INTincScript'][$key]['content']);
+				break;
+			}
+						
+			header("X-ESI-RESPONSE: 1");
+			$EXTconfArr = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['moc_varnish']);
+			$conf = $data['INTincScript'][$key]['conf'];
+			
+			$max_age = $INTiS_cObj->stdWrap($conf['max_age'],$conf['max_age.']);
+			if($max_age ) {
+				header("Cache-control: max-age=".intval($max_age));
+			} elseif(intval($EXTconfArr['userINT_forceTTL'])>0) {
+				header("Cache-control: max-age=".intval($EXTconfArr['userINT_forceTTL']));
+			}			
+			return $incContent;
+		} else {
+			header("X-TYPO3-DISABLE-VARNISHCACHE: true");
+			print date("d/m-Y H:m:i").": Cache for page found, but there is no configuration for USER_INT with hash ".$key." in cache record...".t3lib_div::view_array($data);
+			exit();
+		}		
+	}
+
 		//Copied from tslib_fe
 	/**
 	 * @todo bring to sync with 4.5 code
