9b9ca918637e8ba144d8ef4c44743394def43670
www-data <www-data@mocsystems.com>
2012-07-13 16:34:17 +0200

    [BUGFIX] Set cookie domain to the setting from the install tool
    Also cleans the implementation a bit.
    
    Change-Id: I2aade33cf18902e68d6ea6ce3afb434cced75265
    Fixes: #38877

--- lib/user_writeLoginSessionCookie.php
+++ lib/user_writeLoginSessionCookie.php
@@ -1,21 +1,24 @@
 <?php
+function user_writeLoginSessionCookie($content, $parent) {
+	$cookieName = 'TYPO3_FE_USER_LOGGED_IN';
+	$cookiePath = '/';
 
-function user_writeLoginSessionCookie($content,$parent) {
-	if(t3lib_div::_GP('logintype') === 'login' && is_array($GLOBALS['TSFE']->fe_user->user)) {
+	if (t3lib_div::_GP('logintype') === 'login' && is_array($GLOBALS['TSFE']->fe_user->user)) {
 		$settings = $GLOBALS['TYPO3_CONF_VARS']['SYS'];
 
-		$isRefreshTimeBasedCookie = $parent->isRefreshTimeBasedCookie();
 			// If the cookie lifetime is set, use it:
-		$cookieExpire = ($isRefreshTimeBasedCookie ? $GLOBALS['EXEC_TIME'] + $parent->lifetime : 0);
-			// Use the secure option when the current request is served by a secure connection:
-		$cookieSecure = (bool) $settings['cookieSecure'] && t3lib_div::getIndpEnv('TYPO3_SSL');
-			// Deliver cookies only via HTTP and prevent possible XSS by JavaScript:
-		$cookieHttpOnly = (bool) $settings['cookieHttpOnly'];
+		$cookieExpire = $parent->isRefreshTimeBasedCookie() ? $GLOBALS['EXEC_TIME'] + $parent->lifetime : 0;
 
-		$cookiePath ='/';
+		$cookieDomain = isset($settings['cookieDomain']) ? $settings['cookieDomain'] : '';
+
+			// Use the secure option when the current request is served by a secure connection:
+		$cookieSecure = isset($settings['cookieSecure']) ? ((bool) $settings['cookieSecure'] && t3lib_div::getIndpEnv('TYPO3_SSL')) : FALSE;
+
+			// Deliver cookies only via HTTP and prevent possible XSS by JavaScript:
+		$cookieHttpOnly = isset($settings['cookieHttpOnly']) ? (bool) $settings['cookieHttpOnly'] : FALSE;
 
 		setcookie(
-			'TYPO3_FE_USER_LOGGED_IN',
+			$cookieName,
 			1,
 			$cookieExpire,
 			$cookiePath,
@@ -23,8 +26,7 @@ function user_writeLoginSessionCookie($content,$parent) {
 			$cookieSecure,
 			$cookieHttpOnly
 		);
-	}
-	if(t3lib_div::_GP('logintype') === 'logout') {
-		setcookie('TYPO3_FE_USER_LOGGED_IN',NULL,-1,'/');
+	} elseif (t3lib_div::_GP('logintype') === 'logout') {
+		setcookie($cookieName, NULL, -1, $cookiePath);
 	}
 }
